811cf4acde86bfca4be43a4b616199e7e5b8315a
kbys1025 <kbys1025@gmail.com>
2021-01-29 23:35:35 +0900

    ホーム画面UI変更

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -3,20 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        <div class="col-md-12">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="row">
+                <div class="col-md-3 list-item">
+                    <div class="card">
+                        <img alt="todo_logo" class="card-img-top" src="{{ asset('/img/todo.svg') }}">
+                        <div class="card-body">
+                            <h3 class="card-title">ToDo</h3>
+                            <a href="#" class="stretched-link"></a>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
+                
+                
             </div>
+
+
+
         </div>
     </div>
 </div>
