c3fcd39dc85afa7c9b07f0ad8fcce10da5bb7dfc
kbys1025 <kbys1025@gmail.com>
2021-01-30 09:45:09 +0900

    ホーム画面スタイル修正

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="row">
-                <div class="col-md-3 list-item">
+                <div class="col-md-3 mb-4">
                     <div class="card">
                         <img alt="todo_logo" class="card-img-top" src="{{ asset('/img/todo.svg') }}">
-                        <div class="card-body">
+                        <div class="card-body  mx-auto">
                             <h3 class="card-title">ToDo</h3>
                             <a href="todos" class="stretched-link"></a>
                         </div>
