7208e4236a5dc616ef098b4efc4180592278ad75
kbys1025 <kbys1025@gmail.com>
2021-02-04 20:00:28 +0900

    ToDo詳細　HTML修正

--- backend/resources/views/todo/show.blade.php
+++ backend/resources/views/todo/show.blade.php
@@ -13,26 +13,22 @@
                         <a href="#" class="btn btn-success" style="width:100px;">編集</a>
                         <button href="#" class="btn btn-danger ml-2" style="width:100px;">削除</button>
                     </div>
-                    <div>
+                    <div class="border-bottom mb-3">
                         <label for="todoTitle">タイトル</label>
                         <p id="todoTitle" class="ml-3">{{ $todo->title }}</p>
                     </div>
-                    <hr>
-                    <div>
+                    <div class="border-bottom mb-3">
                         <label for="todoCategory">カテゴリ</label>
                         <p id="todoCategory" class="ml-3">{{ $todo->todo_category->name }}</p>
                     </div>
-                    <hr>
-                    <div>
+                    <div class="border-bottom mb-3">
                         <label for="todoStatus">状態</label>
                         <p id="todoStatus" class="ml-3">@if($todo->status == config('const.todos.STATUS_IN_PROGRESS')) 着手中 @elseif($todo->status == config('const.todos.STATUS_COMPLETED')) 完了 @else 未着手 @endif</p>
                     </div>
-                    <hr>
-                    <div>
+                    <div class="border-bottom mb-3">
                         <label for="todoDeadline">期限</label>
                         <p id="todoDeadline" class="ml-3">@if(isset($todo->deadline)) {{ $todo->deadline->format('Y/m/d') }} @else 未設定 @endif</p>
                     </div>
-                    <hr>
                     <div>
                         <label for="todoText">メモ</label>
                         <p id="todoText" class="ml-3">{!! nl2br(e($todo->text)) !!}</p>
